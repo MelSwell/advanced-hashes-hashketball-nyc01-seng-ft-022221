1f7a220432576d1f845e0f680f8ee6f0c891c89c
Seth Cohen <smcohen82@gmail.com>
2021-02-09 21:42:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,9 @@ def num_points_scored(player_name)
   answer = nil
   game_hash.each do |location, team_details|
     players_array = team_details[:players]
-    
+      players_array.each do |player_details|
+        
+      end
   end
   answer
 end
